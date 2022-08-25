e0314bab305b0fdd437afca842a3a89dc5aa551e
Maya Tomasek <maya.omase@protonmail.com>
2022-08-25 13:03:09 +0200

    Replace GNU Readline with Haskeline

--- src/Main.hs
+++ src/Main.hs
@@ -1,9 +1,11 @@
+{-# LANGUAGE ScopedTypeVariables #-}
 module Main where
+
 import Control.Monad.State (StateT, evalStateT, liftIO, get, put)
 import Core
 import Reader
 import Repl
-import qualified System.Console.Readline as Readline
+import System.Console.Haskeline
 import System.Directory (getHomeDirectory, doesFileExist)
 import System.IO.Error (tryIOError)
 import Types
@@ -17,62 +19,46 @@ import Data.IORef (readIORef)
 import System.Environment (getArgs)
 import Control.Monad
 
-historyFile :: IO String
-historyFile = do
-  home <- getHomeDirectory
-  return $ home ++ "/.local/share/hscheme/history"
+type ScmInteractive a = InputT (ExceptT String (StateT Env IO)) a
 
-loadHistory :: IO ()
-loadHistory = do
-  h <- historyFile
-  exists <- doesFileExist h
-  when exists $ do
-    c <- readFile h
-    mapM_ Readline.addHistory (lines c)
+historyFilePath :: String -> String
+historyFilePath = flip (++) "/.local/share/hscheme/history"
 
-addHistory :: String -> IO ()
-addHistory line = do
-  h <- historyFile
-  _  <- tryIOError (appendFile h (line ++ "\n"))
-  Readline.addHistory line
-
--- Read -> Eval -> Print -> Loop
-repl :: ScmReturn ()
-repl = do
-  line <- liftIO $ Readline.readline "λ> "
-  case line of
-    Nothing -> return ()
-    Just "" -> return ()
-    -- Print current bound symbols and their values
-    Just ":binds" -> do
-      env <- get
+printBinds :: ScmInteractive ()
+printBinds = do
+      env :: Env <- lift get
       mapM_ (\e -> do
                env <- liftIO $ readIORef e
                mapM (\(k,v) -> liftIO $ putStrLn ("(" ++ k ++ " " ++ show v ++ ")")) $ Map.toList env)
         env
+
+-- Read -> Eval -> Print -> Loop
+repl :: ScmInteractive ()
+repl = do
+  line <- getInputLine "λ> "
+  case line of
+    Nothing -> return ()
+    Just "" -> return ()
+    Just ":binds" -> printBinds
     -- Read the input, parse it, evaluate it and print the result
-    Just str -> do
-      liftIO $ addHistory str
-      catchE (do
+    Just str -> lift $ catchE (do
                            v <- re str
                            case v of
                              Nothing -> return ()
-                             Just v -> () <$ scmDisplay v)
-                scmPrint
+                             Just v -> () <$ scmDisplay v) scmPrint
   repl
 
+bind :: [(String, ScmValue)] -> ScmReturn ()
+bind [] = return ()
+bind ((name, val):binds) = insertEnv name val >> bind binds
 
 -- Initialize the environment
-init :: ScmReturn ()
+init :: ScmInteractive ()
 init = do
-  bind prelude
-  re "(load \"lib/Prelude.scm\")"
+  lift $ bind prelude
+  lift $ re "(load \"lib/Prelude.scm\")"
   repl
-    where
-      bind :: [(String, ScmValue)] -> ScmReturn ()
-      bind [] = return ()
-      bind ((name, val):binds) = insertEnv name val >> bind binds
-
+      
 -- Run file
 run :: String -> IO ()
 run str = do
@@ -81,16 +67,19 @@ run str = do
   case result of
     Left e -> error e
     _ -> return ()
-  where
-    bind [] = return ()
-    bind ((key, val):binds) = insertEnv key val >> bind binds
+
+settings home = Settings {
+  complete = completeFilename,
+  historyFile = Just $ historyFilePath home,
+  autoAddHistory = True
+                    }
 
 -- Run in interactive mode
 interactive :: IO ()
 interactive = do
-  loadHistory
   rootEnv <- emptyEnv
-  result <- evalStateT (runExceptT Main.init) rootEnv
+  home <- getHomeDirectory
+  result <- evalStateT (runExceptT $ runInputT (settings home) Main.init) rootEnv
   case result of
     Left e -> putStrLn e
     _ -> return ()
@@ -103,5 +92,3 @@ menu files = mapM_ (\file -> run ("(load \"" ++ file ++ "\")")) files
 
 main :: IO ()
 main = getArgs >>= menu
-
-
